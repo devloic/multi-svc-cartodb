9707413534f415d2e8fdfabef25cfb47d4f66751
Loic Devaux <devloic@gmail.com>
2021-02-05 22:35:32 -0400

    Update provision-pg-10.sh

--- packer/postgis/provisioners/provision-pg-10.sh
+++ packer/postgis/provisioners/provision-pg-10.sh
@@ -120,17 +120,17 @@ PG_APT_FILE="/etc/apt/sources.list.d/pgdg.list"
 
 
 # Create the file repository configuration:
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 # Import the repository signing key:
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 # Update the package lists:
-sudo apt-get update
+apt-get update
 
 # Install the latest version of PostgreSQL.
 # If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
-sudo apt-get -y install postgresql
+apt-get -y install postgresql
 #lolo end
 
 PG_CREATE_CLUSTER_FILE="/etc/postgresql-common/createcluster.conf"
